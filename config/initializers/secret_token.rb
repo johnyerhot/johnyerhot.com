db9c0d64e229cb17074cf60e03ac06b8f214900d
John Yerhot <john@yerhot.org>
2011-10-11 22:46:38 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Johnyerhot::Application.config.secret_token = 'd1480cdcb95f7e46431ab22363de1ee3c1d0f4a907f24ddb342ea59cd6a25f09b9ed537c5d0fe8f2309b54b078a00bd8ab202fe56e7d51fa4e6fc8f4c6cc30da'
